e6e3d5c7bf70ca1ce16ed8ea86f2fd9ee4e4b257
Aleksey Kaptur <102624582+rubybo@users.noreply.github.com>
2022-11-19 21:52:53 +0300

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        user = User(name=name, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return render_template('submit.html')
+
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
